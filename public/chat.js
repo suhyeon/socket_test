1026b899b0ef1f49399314f2e605cfaf2358d4f7
Suhyeon JO <flora12375@gmail.com>
2017-09-18 16:55:10 +0900

    postive update

--- public/chat.js
+++ public/chat.js
@@ -3,7 +3,7 @@ function appendText(messageListEl, text) {
   const messageEl = document.createElement('p')
   messageEl.textContent = text
   messageEl.classList.add('message')
-  messageEl.classList.add('new')
+  messageEl.classList.add('new') //낙관적 업데이트 하는 아이
   messageListEl.insertBefore(messageEl, messageListEl.firstChild)
   return messageEl
 }
@@ -23,19 +23,31 @@ document.addEventListener('DOMContentLoaded', e => {
 //격리시킬 id
   // socket.io 연결 수립하고 room 설정, username 설정
 socket = io('/chat')
-socket.emit('join', {id: roomId})
+socket.emit('join', {id: roomId}, data => {
+  username = data.username
+  console.log(username)
+})
   // form 전송 이벤트 핸들러 => 발생시킬 ‘chat’이라는 e vent를 전송
 formEl.addEventListener('submit', e => {
   e.preventDefault()
   const message = formEl.elements.message.value
   //message = chat.pug에서 input엘리먼트가 된다.
-  socket.emit('clientchat', {message})
+  const messageEl = appendText(messageListEl, formatMessage({username, message}))
+  socket.emit('new chat', {message}, data => {
+    //메시지 전송이 잘되었다는 표시를 해주면된다.
+    messageEl.classList.remove('new')
+  })
+
   formEl.reset()
 })
 
    // (chat) 채팅 메시지가 올 때마다 출력
     socket.on('chat', data => {
-      appendText(messageListEl,data.message)
+      const messageEl = appendText(messageListEl,formatMessage(data))
+      setTimeout(()=>{ //시간지연, 다른 사람들 코드에서 css의 트랜젝션
+        //window.requestAnimationFrame(callback) : setTimeout이랑 비슷한의미이지만 심화과정
+        messageEl.classList.remove('new')
+      })
     })
   // (user connected) 새 사용자가 접속한 사실을 출력
 
--- src/index.js
+++ src/index.js
@@ -127,19 +127,25 @@ chatNsp.on('connection', socket => {
   // 해당 소켓을 room에 연결시킨다.
   // 클라이언트에 username을 보낸다.
   // 유저가 접속했다는 사실을 다른 모든 유저에게 전송한다.
-socket.on('join', data => {
+socket.on('join', (data, ack) => {
   roomId = data.id
   socket.join(roomId)
   //로그인여부를 본인이외의 다른사람들에게 공지 = broadcast
   socket.broadcast.to(roomId).emit('user connected', {username})
+  ack({username})
 })
 
   // chat 이벤트
   // 성공적으로 전송되었다는 사실을 클라이언트에 알림
   // 해당 클라이언트를 제외한 모든 클라이언트에게 메시지 전송
-  socket.on('clientchat', data => {
+  socket.on('new chat', (data, ack) => {
     // 위에는 client가 발생하는 ‘chat’ 밑에는는 server가 발생 ‘chat’
-    chatNsp.to(roomId).emit('chat', data)
+   // chatNsp.to(roomId).emit('serverchat', data)
+    socket.broadcast.to(roomId).emit('chat', {
+      message: data.message,
+      username
+    })
+    ack({ok: true})
   })
 
   // disconnect 내장 이벤트
